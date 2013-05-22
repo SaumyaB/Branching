6b7a87847896d70cde1439da655257edf822081e
Saumya <saumya_bhatnagar@hotmail.com>
2013-05-23 01:41:49 +0530

    Final coding complete by team 2

--- src/Features.java
+++ src/Features.java
@@ -7,6 +7,7 @@ public class Features {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("New feature-1");
+System.out.println("Final changes made by team 2");
 	}
 
 }
